a08e0ed4ea597ef3e067fae0b1fee86587c71db0
Denis <denis.inghelbrecht@outlook.be>
2018-09-12 16:28:19 +0200

    Finished modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,8 +1,10 @@
 import MobileMenu from './modules/mobileMenu';
 import RevealOnScroll from './modules/revealOnScroll';
 import StickyHeader from './modules/stickyHeader';
+import Modal from './modules/modal';
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll(".feature-item", "85%");
 new RevealOnScroll(".testimonial", "60%");
 const stickyHeader = new StickyHeader(".large-hero__title");
+const modal = new Modal();
